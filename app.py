ef6a5a0e86365ac0ad33a82cf2c841f1f7bd6c14
cgmsync <roth.lyds18@gmail.com>
2025-09-09 13:17:29 -0700

    Initial commit of Solistic Health project

--- app.py
+++ app.py
@@ -191,6 +191,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
     app.run(debug=True)
